325380c5bf56c3c7e4dd9c1561326dfc316231b1
Albina <albinabist1993@mail.ru>
2021-08-26 17:17:20 +0300

    Разделила стили на файлы. Начала делать адаптив

--- src/js/swiper1.js
+++ src/js/swiper1.js
@@ -1,11 +1,3 @@
-var swiper = new Swiper(".mySwiper1", {
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination-1",
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + "</span>";
-    }
-  },
+const swiper = new Swiper(".mySwiper1", {
+
 });
-  
